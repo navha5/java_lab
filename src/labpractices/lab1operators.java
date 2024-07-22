1f96518d08eedca9fd7b9124f63fdbc599460f8e
navne <navhneetwadhwa@gmail.com>
2024-07-22 18:07:40 +0100

    day 1

---
+++ src/labpractices/lab1operators.java
@@ -0,0 +1,92 @@
+package labpractices;
+
+public class lab1operators {
+    public static void main(String[] args) {
+        //---------------opertors--------------------
+
+        //1. Arithmetic operator(+,-,*,/,%)to perform mathmetical operations
+//       int a = 20;
+//       int b = 3;
+//       int c =a;
+//       a = b;
+//       b =c;
+
+
+
+//        System.out.println(a+b);
+//        System.out.println(a-b);
+//        System.out.println(a*b);
+//        System.out.println(a/b);
+//        System.out.println(a%b);
+        //----------concatanation------
+
+//        System.out.println("sum of a and b is :" + (a+b));
+//        System.out.println("sum of a and b is :" + (a-b));
+//        System.out.println("sum of a and b is :" + (a*b));
+//        System.out.println("sum of a and b is :" + (a/b));
+//        System.out.println("sum of a and b is :" + (a%b));
+        //System.out.println(c);
+
+        //2.-Relational operators(==,>,<,<=,>=,!=) gives boolean values true /false
+//        System.out.println(a==b);
+//        System.out.println(a<b);
+//        System.out.println(a>b);
+//        System.out.println(a<=b);
+//        System.out.println(a>=b);
+//        System.out.println(a!=b);
+
+        //logical operators(&&,||,!)
+        //boolean x = false;
+       // boolean y = true;
+
+        //System.out.println(x&&y);
+        //System.out.println(x||y);
+       // System.out.println(!y);
+
+        //--increment and decrement operators (++,--)
+
+        //a =a+1;,(increment way 1)
+        //a++; (increment way 2)
+       // a=a-1;(decrement way 2)
+       // a--;(decrement way 2)
+
+              // System.out.println(a);
+        //---post increment--(it will give same 20 value becaz first it will assign x value to result.
+        //int x = 20;
+//        int result =x++;
+//        System.out.println(result);
+        //pre increment (it will store increment first and gives 21)
+       // int result =++x;
+        //System.out.println(result);
+
+        //---post decrement--(it will give same 20 value becaz first it will assign x value to result.
+        //int x = 20;
+//       int result =x--;
+//       System.out.println(result);
+        //pre decrement (it will store decrement first and gives 19)
+         //int result =--x;
+        //System.out.println(result);
+
+        //assignment operators(=,(to assign the value to variable only)
+
+        //how to swap two values---
+
+
+       //System.out.println("after swap value of a is :" + a);
+       // System.out.println("after swap value of a is :" + b);
+
+
+        int a =28;
+        int b = 5;
+        int c = a;
+        a=b;
+        b=c;
+        System.out.println("after swap a is" + a);
+
+
+
+
+
+
+    }
+}
