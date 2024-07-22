59abe5ff7c557163a4b88b23175f1505f6e49f2c
navne <navhneetwadhwa@gmail.com>
2024-07-22 18:12:17 +0100

    day 1

---
+++ src/labpractices/lab2controlstatements.java
@@ -0,0 +1,72 @@
+package labpractices;
+
+public class lab2controlstatements {
+    public static void main(String[] args) {
+        //how to use % remainder
+//       double a= 34;
+//        double b = 10;
+//        double result =a%b;
+//        System.out.println(result);
+////
+//        if(a>=b);
+//        System.out.println("yes");
+
+        //----------escape words-----
+       // char c = '\n';//(add new/nextline)
+//        char c1 = '\t';(add tab )
+//        char c2 = '\b';(backlash delete 1 character)
+//        char c3 = '\r';(backlash delete 1 word)
+//        System.out.println("navhneet"+c+"wadhwa");
+//        System.out.println("navhneet"+c1+"wadhwa");
+//        System.out.println("navhneet"+c2+"wadhwa");
+//        System.out.println("navhneet"+c3+"wadhwa");
+
+        //-urinary operator---
+//        int a =4;
+//        int b = -2;
+//        int c =a*b;
+//        System.out.println(c);
+//        int merce = -1;
+//        System.out.println(merce);
+
+       // System.out.println("nav " + c + "wadhwa");
+
+        //------------compound assignment  operators (+=,-=,*=,/=,%=)------
+
+       //int a = 10;
+       // a+=9;
+      // a-=9;
+       // a*=9;
+//        a/=2;
+//       a%=3;
+       // a++;
+        //System.out.println(a);
+
+        //not (!) ----------- logical operator--------
+//        System.out.println(!(10>20));--true
+//        System.out.println(!!(10>20));--false
+//        System.out.println(!!!(10>20));--true
+//        System.out.println(!!!!(10>20));---false
+//        System.out.println((10>20));----false
+
+        //---------OR(||) logical oprator----------
+//        System.out.println(true||true);----true
+//        System.out.println(true||false);---true     -----becaz if anyone is true from both it gives true
+//        System.out.println(false||true);----true
+//        System.out.println(false||false);---false   ------if both are false then it gives false
+
+        //---------and gate logical operator (&&)------
+
+//        System.out.println(true&&true);---true becaz both same
+//       System.out.println(true&&false);--false  becaz one is false for this both should be matched
+//       System.out.println(false&&true);--------false
+//      System.out.println(false&&false);--false
+
+        //--------inteview question----
+        //int a =12;
+       // System.out.println(!(a>10||a<5));---false
+
+
+
+    }
+}
