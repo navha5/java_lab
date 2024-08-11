5809ca58dfced03fa61064e62fbecaea746ade2f
navne <navhneetwadhwa@gmail.com>
2024-08-11 20:19:46 +0100

    lab5_4

---
+++ src/lab5_21724/lab5_4.java
@@ -0,0 +1,52 @@
+package lab5_21724;
+
+public class lab5_4 {
+    public static void main(String[] args) {
+
+//        for (double f = 0.1; f <= 10.6;f++) {
+//            System.out.println(f);
+//
+//           if(f==7.1)
+         {
+               //break;
+//             for (int i = 0; i < 10; i++) {
+//                 System.out.println(i);--result 12345 becaz i put sout inside of loop
+//                 if(i==5){
+//                     break;
+//             for (int i = 0; i < 10; i++) {
+//                 if(i==5){
+//                     break;   ---result is 1234 becaz i put sout outside of loop
+//
+//                 }
+//                 System.out.println(i);
+             //for odd numbers
+//             for (int i = 0; i <= 50; i++) {
+//                 if(i%2==0){
+//                     System.out.println(i);
+//            for even numbers
+//             for (int i = 0; i <=50; i++) {
+//                 if(i%2!=0){
+//                     System.out.println(i);
+
+                 }
+
+             }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
